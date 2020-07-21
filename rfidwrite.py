0f4f933cca922ad5c91d20e9ab4008735d351eef
Adrian Spittles <adrian@spittles.com>
2020-07-21 08:22:57 +0100

    Created RFID Write example

---
+++ rfidwrite.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from time import sleep
+import RPi.GPIO as GPIO
+import sys
+sys.path.append('/home/pi/MFRC522-python')
+from mfrc522 import SimpleMFRC522
+reader = SimpleMFRC522()
+
+try:
+        text = raw_input('New data:')
+        print("Now place your tag to write")
+        reader.write(text)
+        print("Written")
+finally:
+        GPIO.cleanup()
