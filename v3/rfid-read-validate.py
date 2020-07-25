2f9a7a4803cfb181693192837ef3a088176a4b8a
Adrian Spittles <adrian@spittles.com>
2020-07-25 22:26:12 +1000

    Defined functions

--- v3/rfid-read-validate.py
+++ v3/rfid-read-validate.py
@@ -1,37 +1,53 @@
-#  "config": {
-#    "log_level": "DEBUG",
-#    "log_file": "~/pi-rfid-py-post/v3/rfid-read-validate.log"
-#    "reader": "RF522" }
-# rfid-door-lock.json  rfid-read-validate.log  rfid-read-validate.py  rfid.sh
-
 from time import sleep
 import RPi.GPIO as GPIO
 import sys
 import json
 import logging
-
 sys.path.append('/home/pi/MFRC522-python')
 from mfrc522 import SimpleMFRC522
 
-with open('rfid-door-lock.json') as f:
-  data = json.load(f)
-
-logging.basicConfig(filename=(data["config"]["log_file"]),level=logging.DEBUG,format='%(asctime)s %(levelname)s:%(message)s')
-
-reader = SimpleMFRC522()
-
-try:
+# Function to Read the RFID card
+def rfid_read():
   print("Hold a tag near the reader")
   uid, text = reader.read()
-  print("Key UID: %s" % (uid))
+  return uid;
 
+# Function to Validate the Card UID against JSON list of allowed users
+def validate_access( uid, data ):
   try:
-    print((data["users"][str(uid)]["firstName"]) + " " + (data["users"][str(uid)]["lastName"]) + " - Status: " + str(data["users"][str(uid)]["active"]))
-    logging.info("Access by:" + (data["users"][str(uid)]["firstName"]) + " " + (data["users"][str(uid)]["lastName"]))
-    print(data["users"][str(uid)])
+    name = (data["users"][str(uid)]["firstName"]) + " " + (data["users"][str(uid)]["lastName"])
+    authorised = (data["users"][str(uid)]["active"])
+    if authorised:
+      logging.info("Access by:" + name)
+    else:
+      logging.warning("Deactivated card attempt by: " + str(uid) + " (" + name + ")")
+    # print(name + " " + str(status))
+    # print((data["users"][str(uid)]["firstName"]) + " " + (data["users"][str(uid)]["lastName"]) + " - Status: " + str(data["users"][str(uid)]["active"]))
+    #print(data["users"][str(uid)])
   except KeyError:
     print("ID doesn't exist")
     logging.warning("Access attempt by: " + str(uid))
-finally:
-#  sleep(2)
+    authorised = False
+
+  return authorised;
+
+# Read the config file and store in memory
+with open('rfid-door-lock.json') as f:
+  data = json.load(f)
+
+# Enable & configure logging
+logging.basicConfig(filename=(data["config"]["log_file"]),level=logging.DEBUG,format='%(asctime)s %(levelname)s:%(message)s')
+
+# Create RFID reader object
+reader = SimpleMFRC522()
+
+# Main Loop
+try:
+  while True:
+    uid = rfid_read();
+    authorised = validate_access( uid, data );
+    print(str(authorised))
+    sleep(5);
+
+except KeyboardInterrupt:
   GPIO.cleanup()
